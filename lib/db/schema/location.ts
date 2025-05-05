14835900e0ca6c9b0d7f15fab3a0c1169cf256dd
Salomon Romano <salo_romano@yahoo.com>
2025-05-05 11:17:04 -0600

    :sparkles: add location page (#60)

--- lib/db/schema/location.ts
+++ lib/db/schema/location.ts
@@ -1,4 +1,6 @@
 import { int, real, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { createInsertSchema } from "drizzle-zod";
+import { z } from "zod";
 
 import { user } from "./auth";
 
@@ -15,4 +17,17 @@ export const location = sqliteTable("location", {
     .notNull()
     .$default(() => Date.now())
     .$onUpdate(() => Date.now()),
+});
+
+export const InsertLocation = createInsertSchema(location, {
+  name: field => field.min(1).max(100),
+  description: field => field.max(100),
+  lat: z.coerce.number().min(-90).max(90),
+  long: z.coerce.number().min(-180).max(180),
+}).omit({
+  id: true,
+  slug: true,
+  userId: true,
+  createdAt: true,
+  updatedAt: true,
 });
---
+++ pages/dashboard/add.vue
@@ -0,0 +1,128 @@
+<script lang="ts" setup>
+import type { FetchError } from "ofetch";
+
+import { toTypedSchema } from "@vee-validate/zod";
+
+import { InsertLocation } from "~/lib/db/schema";
+
+const { $csrfFetch } = useNuxtApp();
+const loading = ref(false);
+const router = useRouter();
+const submitError = ref();
+const submitted = ref(false);
+
+const { handleSubmit, errors, meta, setErrors } = useForm({
+  validationSchema: toTypedSchema(InsertLocation),
+});
+
+const onSubmit = handleSubmit(async (values) => {
+  try {
+    submitError.value = "";
+    loading.value = true;
+    await $csrfFetch("/api/locations", {
+      method: "POST",
+      body: values,
+    });
+    submitted.value = true;
+    navigateTo("/dashboard");
+  }
+  catch (e) {
+    const error = e as FetchError;
+    if (error.data?.data) {
+      setErrors(error.data?.data);
+    }
+    submitError.value = error.statusMessage || "An unknown error occurred.";
+  }
+  finally {
+    loading.value = false;
+  }
+});
+
+onBeforeRouteLeave(() => {
+  if (!submitted.value && meta.value.dirty) {
+    // eslint-disable-next-line no-alert
+    const confirm = window.confirm("Are you sure you want to leave? All unsaved changes will be lost.");
+    if (!confirm) {
+      return false;
+    }
+  }
+  return true;
+});
+</script>
+
+<template>
+  <div class="container max-w-md mx-auto">
+    <div class="my-4">
+      <h1 class="text-lg">
+        Add Location
+      </h1>
+      <p class="text-sm">
+        A location is a place you have traveled or will travel to. It can be a city, country, state or point of interest. You can add specific times you visited this location after adding it.
+      </p>
+    </div>
+    <div v-if="submitError" role="alert" class="alert alert-error">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6 shrink-0 stroke-current"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+        />
+      </svg>
+      <span>{{ submitError }}</span>
+    </div>
+    <form class="flex flex-col gap-2" @submit.prevent="onSubmit">
+      <AppFormField
+        label="Name"
+        name="name"
+        :error="errors.name"
+        :disabled="loading"
+      />
+
+      <AppFormField
+        label="Description"
+        name="description"
+        type="textarea"
+        :error="errors.description"
+        :disabled="loading"
+      />
+
+      <AppFormField
+        label="Latitude"
+        name="lat"
+        :error="errors.lat"
+        :disabled="loading"
+      />
+
+      <AppFormField
+        label="Longitude"
+        name="long"
+        :error="errors.long"
+        :disabled="loading"
+      />
+
+      <div class="flex justify-end gap-2">
+        <button
+          :disabled="loading"
+          type="button"
+          class="btn btn-outline"
+          @click="router.back()"
+        >
+          <Icon name="tabler:arrow-left" size="24" />
+          Cancel
+        </button>
+        <button :disabled="loading" type="submit" class="btn btn-primary">
+          Add
+          <span v-if="loading" class="loading loading-spinner loading-sm" />
+          <Icon v-else name="tabler:circle-plus-filled" size="24" />
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
