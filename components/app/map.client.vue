6e5a011d2f71999a4c1e253da1aba2822bf4dfc3
Salomon Romano <salo_romano@yahoo.com>
2025-05-05 20:39:22 -0600

    :sparkles: show points on map

--- components/app/map.client.vue
+++ components/app/map.client.vue
@@ -2,15 +2,43 @@
 import { CENTER_WORLD } from "~/lib/constants";
 
 const colorMode = useColorMode();
+const mapStore = useMapStore();
+
 const style = computed(() => colorMode.value === "dark"
   ? "/styles/dark.json"
   : "https://tiles.openfreemap.org/styles/liberty");
 const center = CENTER_WORLD;
 const zoom = 1;
+
+onMounted(() => {
+  mapStore.init();
+});
 </script>
 
 <template>
-  <MglMap :map-style="style" :center="center" :zoom="zoom">
+  <MglMap
+    :map-style="style"
+    :center="center"
+    :zoom="zoom"
+  >
     <MglNavigationControl />
+    <MglMarker
+      v-for="point in mapStore.mapPoints"
+      :key="point.id"
+      :coordinates="[point.long, point.lat]"
+    >
+      <template #marker>
+        <div
+          class="tooltip tooltip-top"
+          :data-tip="point.label"
+        >
+          <Icon
+            name="tabler:map-pin-filled"
+            size="24"
+            class="text-secondary"
+          />
+        </div>
+      </template>
+    </MglMarker>
   </MglMap>
 </template>
--- lib/constants.ts
+++ lib/constants.ts
@@ -1,1 +1,3 @@
-export const CENTER_WORLD = [-39.511220, 35.008598];
+import type { LngLatLike } from "maplibre-gl";
+
+export const CENTER_WORLD = [-39.511220, 35.008598] as LngLatLike;
