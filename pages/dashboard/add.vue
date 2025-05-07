6888230e69850eb226aab5b7dbec08ddbcaca974
Salomon Romano <salo_romano@yahoo.com>
2025-05-06 20:44:07 -0600

    :sparkles: add movable marker to map (#68)

--- pages/dashboard/add.vue
+++ pages/dashboard/add.vue
@@ -3,6 +3,7 @@ import type { FetchError } from "ofetch";
 
 import { toTypedSchema } from "@vee-validate/zod";
 
+import { CENTER_WORLD } from "~/lib/constants";
 import { InsertLocation } from "~/lib/db/schema";
 
 const { $csrfFetch } = useNuxtApp();
@@ -10,9 +11,23 @@ const loading = ref(false);
 const router = useRouter();
 const submitError = ref();
 const submitted = ref(false);
+const mapStore = useMapStore();
 
-const { handleSubmit, errors, meta, setErrors } = useForm({
+const {
+  handleSubmit,
+  errors,
+  meta,
+  controlledValues,
+  setErrors,
+  setFieldValue,
+} = useForm({
   validationSchema: toTypedSchema(InsertLocation),
+  initialValues: {
+    name: "",
+    description: "",
+    lat: (CENTER_WORLD as [number, number])[1],
+    long: (CENTER_WORLD as [number, number])[0],
+  },
 });
 
 const onSubmit = handleSubmit(async (values) => {
@@ -40,6 +55,30 @@ const onSubmit = handleSubmit(async (values) => {
   }
 });
 
+function formatNumber(value?: number) {
+  if (!value) {
+    return 0;
+  }
+  return value.toFixed(5);
+}
+
+effect(() => {
+  if (mapStore.addedPoint) {
+    setFieldValue("long", mapStore.addedPoint.long);
+    setFieldValue("lat", mapStore.addedPoint.lat);
+  }
+});
+
+onMounted(() => {
+  mapStore.addedPoint = {
+    id: 1,
+    name: "Added",
+    description: "",
+    lat: (CENTER_WORLD as [number, number])[1],
+    long: (CENTER_WORLD as [number, number])[0],
+  };
+});
+
 onBeforeRouteLeave(() => {
   if (!submitted.value && meta.value.dirty) {
     // eslint-disable-next-line no-alert
@@ -48,12 +87,13 @@ onBeforeRouteLeave(() => {
       return false;
     }
   }
+  mapStore.addedPoint = null;
   return true;
 });
 </script>
 
 <template>
-  <div class="container max-w-md mx-auto">
+  <div class="container max-w-md mx-auto p-4">
     <div class="my-4">
       <h1 class="text-lg">
         Add Location
@@ -99,19 +139,11 @@ onBeforeRouteLeave(() => {
         :disabled="loading"
       />
 
-      <AppFormField
-        label="Latitude"
-        name="lat"
-        :error="errors.lat"
-        :disabled="loading"
-      />
-
-      <AppFormField
-        label="Longitude"
-        name="long"
-        :error="errors.long"
-        :disabled="loading"
-      />
+      <p>Drag the <Icon name="tabler:map-pin-filled" class="text-warning" /> marker to your desired location</p>
+      <p>Or double click on the map</p>
+      <p class="text-xs text-gray-400">
+        Current location: {{ formatNumber(controlledValues.lat) }}, {{ formatNumber(controlledValues.long) }}
+      </p>
 
       <div class="flex justify-end gap-2">
         <button
