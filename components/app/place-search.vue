9f5aa5559d16efb1d3d7cf80dc3602411e6eac8c
Salomon Romano <salo_romano@yahoo.com>
2025-05-08 14:24:39 -0600

    :sparkles: add location search (#70)

---
+++ components/app/place-search.vue
@@ -0,0 +1,150 @@
+<script lang="ts" setup>
+import type { FetchError } from "ofetch";
+
+import type { NominatimResult } from "~/lib/types";
+
+import { SearchSchema } from "~/lib/zod-schemas";
+
+const emit = defineEmits<{
+  resultSelected: [result: NominatimResult];
+}>();
+
+const searchResults = ref<NominatimResult[]>([]);
+const form = useTemplateRef("form");
+const loading = ref(false);
+const hasSearched = ref(false);
+const errorMessage = ref("");
+
+async function onSubmit(query: Record<string, string>) {
+  try {
+    loading.value = true;
+    hasSearched.value = true;
+    errorMessage.value = "";
+    searchResults.value = [];
+    const results = await $fetch("/api/search", {
+      query,
+    });
+
+    searchResults.value = results;
+  }
+  catch (e) {
+    const error = e as FetchError;
+    errorMessage.value = getFetchErrorMessage(error);
+  }
+  finally {
+    loading.value = false;
+  }
+}
+
+function setLocation(result: NominatimResult) {
+  emit("resultSelected", result);
+  searchResults.value = [];
+  errorMessage.value = "";
+  hasSearched.value = false;
+  if (form.value) {
+    form.value.resetForm();
+  }
+}
+</script>
+
+<template>
+  <div class="flex flex-col gap-2">
+    <Form
+      ref="form"
+      v-slot="{ errors }"
+      class="flex flex-col gap-2 items-center"
+      :validation-schema="toTypedSchema(SearchSchema)"
+      :initial-values="{ q: '' }"
+      @submit="onSubmit"
+    >
+      <div class="join">
+        <div>
+          <label class="input join-item">
+            <Icon name="tabler:search" />
+            <Field
+              type="text"
+              name="q"
+              placeholder="Search for a location..."
+              :class="{ 'input-error': errors.q }"
+              :disabled="loading"
+            /></label>
+          <div v-if="errors.q" class="validator-hint text-error">
+            {{ errors.q }}
+          </div>
+        </div>
+        <button class="btn btn-neutral join-item" :disabled="loading">
+          Search
+        </button>
+      </div>
+    </Form>
+
+    <div
+      v-if="!loading && errorMessage"
+      role="alert"
+      class="alert alert-error"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="size-5"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+        />
+      </svg>
+      {{ errorMessage }}
+    </div>
+
+    <div
+      v-if="!loading && hasSearched && !searchResults.length"
+      role="alert"
+      class="alert alert-warning"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="size-5"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+        />
+      </svg>
+      No results found.
+    </div>
+    <div v-if="loading" class="flex justify-center">
+      <div class="loading loading-lg" />
+    </div>
+    <div class="flex flex-col overflow-auto gap-2 max-h-52 mt-2">
+      <div
+        v-for="result in searchResults"
+        :key="result.place_id"
+        class="card card-sm bg-base-100"
+      >
+        <div class="card-body">
+          <h4 class="card-title">
+            {{ result.display_name }}
+          </h4>
+          <div class="justify-end card-actions">
+            <button
+              class="btn btn-sm btn-warning"
+              @click="setLocation(result)"
+            >
+              Set Location
+              <Icon name="tabler:map-pin-share" size="20" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ utils/format-number.ts
@@ -0,0 +1,7 @@
+export function formatNumber(value?: number) {
+  if (!value) {
+    return 0;
+  }
+
+  return value.toFixed(5);
+}
