e7282d067ef2eb52b06e613720280ad55fc66663
Salomon Romano <salo_romano@yahoo.com>
2025-05-14 11:35:29 -0600

    :sparkles: show location log cards (#78)

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,8 +1,10 @@
 <script lang="ts" setup>
 const locationsStore = useLocationStore();
-const mapStore = useMapStore();
 
-const { locations, locationsStatus: status } = storeToRefs(locationsStore);
+const {
+  locations,
+  locationsStatus: status,
+} = storeToRefs(locationsStore);
 
 onMounted(() => {
   locationsStore.refreshLocations();
@@ -10,7 +12,7 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="p-4 min-h-64">
+  <div class="page-content-top">
     <h2 class="text-2xl">
       Locations
     </h2>
@@ -20,32 +22,13 @@ onMounted(() => {
 
     <div
       v-else-if="locations && locations.length > 0"
-      class="flex flex-nowrap mt-4 gap-2 overflow-auto"
+      class="location-list"
     >
-      <NuxtLink
+      <LocationCard
         v-for="location in locations"
         :key="location.id"
-        class="card card-compact bg-base-300 h-40 border-2 w-72 mb-2 shrink-0 cursor-pointer"
-        :class="{
-          'border-accent': isPointSelected(location, mapStore.selectedPoint),
-          'border-transparent': !isPointSelected(location, mapStore.selectedPoint),
-        }"
-        :to="{
-          name: 'dashboard-location-slug',
-          params: {
-            slug: location.slug,
-          },
-        }"
-        @mouseenter="mapStore.selectedPoint = createMapPointFromLocation(location)"
-        @mouseleave="mapStore.selectedPoint = null"
-      >
-        <div class="card-body">
-          <h3 class="text-xl">
-            {{ location.name }}
-          </h3>
-          <p>{{ location.description }}</p>
-        </div>
-      </NuxtLink>
+        :map-point="createMapPointFromLocation(location)"
+      />
     </div>
 
     <div v-else class="flex flex-col gap-2 mt-4">
