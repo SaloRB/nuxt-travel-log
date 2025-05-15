e709e350bee4abb2086a108ea4b5db8a55b274a8
Salomon Romano <salo_romano@yahoo.com>
2025-05-15 12:14:33 -0600

    :sparkles: edit location log

--- pages/dashboard/location/[slug]/[id]/edit.vue
+++ pages/dashboard/location/[slug]/[id]/edit.vue
@@ -1,5 +1,42 @@
+<script lang="ts" setup>
+import type { InsertLocationLog } from "~/lib/db/schema";
+
+const route = useRoute();
+const { $csrfFetch } = useNuxtApp();
+
+const locationStore = useLocationStore();
+const {
+  currentLocationLog: locationLog,
+} = storeToRefs(locationStore);
+
+async function onSubmit(values: InsertLocationLog) {
+  await $csrfFetch(
+    `/api/locations/${route.params.slug}/${route.params.id}`,
+    {
+      method: "PUT",
+      body: values,
+    },
+  );
+}
+
+function onSubmitComplete() {
+  navigateTo({
+    name: "dashboard-location-slug-id",
+    params: {
+      slug: route.params.slug,
+      id: route.params.id,
+    },
+  });
+}
+</script>
+
 <template>
-  <h2>
-    Log Edit Page
-  </h2>
+  <LocationLogForm
+    v-if="locationLog"
+    submit-label="Update Location Log"
+    submit-icon="tabler:map-pin-up"
+    :on-submit="onSubmit"
+    :on-submit-complete="onSubmitComplete"
+    :initial-values="locationLog"
+  />
 </template>
